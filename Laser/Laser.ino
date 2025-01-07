b9dde48a923f1867b34e2474f5ca78ac6f956182
Victor López <150093625+VictorLopezG@users.noreply.github.com>
2025-01-07 14:24:54 -0300

    Add files via upload

---
+++ Laser/Laser.ino
@@ -0,0 +1,205 @@
+#include "Adafruit_VL53L0X.h"
+#include <RTClib.h>
+#include <Wire.h>
+#include <math.h>
+#include "RTClib.h"
+#include <Adafruit_MLX90614.h>
+#include <Adafruit_ADS1X15.h>
+#include <SoftwareSerial.h>
+#include <CRC16.h>
+#include <SoftwareSerial.h>
+#include <SD.h>
+//puertos para el laser grande
+#define TX_laser 17
+#define RX_laser 16
+//sensor de temperatura
+#define MLX_ADDRESS 0x5A
+//laser
+#define VL53_ADDRESS 0x29
+//modulo ads
+#define ADS1_ADDRESS 0x48
+//reloj
+RTC_DS1307 DS1307_RTC;
+SoftwareSerial mySerial(TX_laser, RX_laser);
+//comprobacion de datos
+CRC16 crc(CRC16_MODBUS_POLYNOME,
+          CRC16_MODBUS_INITIAL,
+          CRC16_MODBUS_XOR_OUT,
+          CRC16_MODBUS_REV_IN,
+          CRC16_MODBUS_REV_OUT);
+
+float Temp;
+VL53L0X_RangingMeasurementData_t measure1;
+int a = 0;
+int b = 0;
+bool led = true;
+
+Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
+//inicialización de la biblioteca modulo ads y sensor temp
+Adafruit_ADS1115 ads;
+Adafruit_MLX90614 mlx = Adafruit_MLX90614();
+//comandos para comunicacion con laser grande
+uint8_t byte6_1, byte5_1, byte6_2, byte5_2, byte5_1_hex, byte5_2_hex, byte6_1_hex, byte6_2_hex;
+
+uint8_t commandContinue[8] = { 0x01, 0x03, 0x00, 0x01, 0x00, 0x02, 0x95, 0xCB };
+uint8_t commandUnique[8] = { 0x01, 0x03, 0x00, 0x0F, 0x00, 0x02, 0xF4, 0x08 };
+uint8_t marche[11] = { 0x01, 0x10, 0x00, 0x03, 0x00, 0x01, 0x02, 0x00, 0x01, 0x67, 0xA3 };
+uint8_t data[9];
+uint32_t distance;
+//calculo de la distancia
+uint32_t calcul_distance(uint8_t a, uint8_t b, uint8_t c, uint8_t d) {
+  distance = (d * 1 + c * 16 + b * 16 * 16 + a * 16 * 16 * 16);  //converts the 4 hex digits into decimal
+  return distance;
+}
+//iniciar reloj
+void RTC_init() {
+  DS1307_RTC.begin();
+  Serial.println(!DS1307_RTC.isrunning() ? "RTC no funciona" : "RTC funcionando");
+  if (!DS1307_RTC.isrunning()) {
+    Serial.println("Ajustando el reloj interno...");
+    DS1307_RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    a = 1;
+  } else {
+    b = 1;
+  }
+  Serial.flush();
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+  File file = fs.open(path, FILE_WRITE);
+  Serial.println(!file ? "Archivo no se pudo abrir" : "Archivo abierto");
+  Serial.println(file.print(message) ? "Archivo escrito" : "No se ha podido escribir");
+  file.close();
+}
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+  File file = fs.open(path, FILE_APPEND);
+  Serial.print(!file ? "Archivo no se pudo abrir\n":"");
+  Serial.print(!file.print(message) ? "No se ha podido añadir\n":"");
+  file.close();
+}
+//iniciar SD
+void SD_init() {
+  Serial.println(!SD.begin() ? "No se puede encontrar la SD" : "Se encontro la SD");
+  File file = SD.open("/Datos.txt");
+  Serial.println(!file ? "El archivo no existe, se creara uno" : "El archivo ya existe");
+  if (!file) {
+    writeFile(SD, "/Datos.txt", "-WPMS5003\r\n");
+  } else if (file) {
+    appendFile(SD, "/Datos.txt", "-APMS5003\r\n");
+  }
+  file.close();
+}
+
+String getDate() {
+  DateTime currentDateTime = DS1307_RTC.now();
+  String dateTime = String(currentDateTime.timestamp(DateTime::TIMESTAMP_DATE)) + " " + String(currentDateTime.timestamp(DateTime::TIMESTAMP_TIME));
+  // Serial.println(dateTime);
+  return dateTime;
+}
+
+void setup() {
+  Serial.begin(115200);
+  mySerial.begin(9600);
+  Serial.print("start");
+  mySerial.write(marche, 11);
+  Wire.begin();
+  lox1.begin(VL53_ADDRESS);
+  mlx.begin(MLX_ADDRESS);
+  ads.begin(ADS1_ADDRESS);
+  pinMode(14, OUTPUT);
+
+  RTC_init();
+
+  if (!SD.begin(5)) {
+    Serial.println("Initialization SD failed!");
+    return;
+  } else {
+    Serial.println("Initialization SD done.");
+  }
+  if (!SD.exists("/Datos.txt")) {
+    File myFile = SD.open("/Datos.txt", FILE_WRITE);
+    myFile.println("Fecha Hora Temperatura Distancia Laser_pequeño Paso_Madera");
+    myFile.close();
+  }
+}
+
+unsigned long timer = 0;
+
+void loop() {
+
+  if (millis() - timer > 1000) {
+    mySerial.write(commandUnique, 8);
+    // delay(50);
+    if (mySerial.available() > 0) {
+      mySerial.readBytes(data, 9);
+      mySerial.overflow();
+    }
+    Temp = mlx.readObjectTempC();  //Temperatura obtenida del sensor de temperatura
+    Serial.print("Temperatura Objeto: ");
+    // Serial.println(Temp);
+    lox1.rangingTest(&measure1, true);
+
+    for (int i = 0; i < sizeof(data); i++) {
+      crc.add(data[i]);
+    }
+    uint8_t CRC = crc.calc();
+    //Serial.println(CRC, HEX);
+
+    // delay(50);
+    byte5_1 = data[5] / 16;
+    byte5_2 = data[5] - byte5_1 * 16;
+    byte6_1 = data[6] / 16;
+    byte6_2 = data[6] - byte6_1 * 16;
+    distance = calcul_distance(byte5_1, byte5_2, byte6_1, byte6_2);
+
+    crc.restart();
+    timer = millis();
+    String fecha = getDate();
+
+    int ValidadorMadera;
+    //verificar paso de la madera
+    if (measure1.RangeMilliMeter > 198) {
+      ValidadorMadera = 0;
+    } else {
+      ValidadorMadera = 1;
+    }
+    //parpadeo del led
+    if (led == true) {
+      digitalWrite(14, HIGH);
+    } else {
+      digitalWrite(14, LOW);
+    }
+    led = !led;
+
+    int contador;
+    //visualizar y guardar los datos
+    if (distance != 0 && !isnan(Temp) && distance < 20000 && CRC == 0) {
+
+      Serial.println("------- Datos del Sensor -------");
+      Serial.print("Fecha: ");
+      Serial.println(fecha);  // Asegúrate de que 'fecha' esté definida y tenga un formato válido
+      Serial.print("Temperatura: ");
+      Serial.print(Temp);
+      Serial.println(" ºC");
+      Serial.print("Distancia: ");
+      Serial.print(distance);
+      Serial.println(" mm");
+      Serial.print("Medidas Laser Pequeño: ");
+      Serial.print(measure1.RangeMilliMeter);
+      Serial.println(" mm");
+      Serial.print("Pasando Madera: ");
+      Serial.println(ValidadorMadera);
+      String temperatura = String(Temp);
+      temperatura.replace('.', ',');
+      String datosSD = fecha + " " + String(temperatura) + " " + String(distance) + " " + String(measure1.RangeMilliMeter) + " " + String(ValidadorMadera) + "\n";
+      appendFile(SD, "/Datos.txt", datosSD.c_str());
+    } else {
+      Serial.println("Datos se estan recibiendo mal");
+      Serial.println("Distancia= " + String(distance) + " Temperatura = " + String(Temp) + " CRC =" + String(CRC));
+      contador++;
+      if (contador >= 35) {
+        ESP.restart();
+      }
+    }
+  }
+}
